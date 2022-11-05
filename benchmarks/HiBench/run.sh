aee7459f06910519a0da3f6861176ebd765f2679
Ubuntu <ubuntu@ip-172-31-12-13.us-east-2.compute.internal>
2022-11-05 19:43:43 +0000

    formulation

---
+++ benchmarks/HiBench/run.sh
@@ -0,0 +1,11 @@
+# cp (and remove existing) files for spark.conf and hadoop.conf.
+
+# start hadoop, spark, and yarn instance.
+
+# run sparkbench with pure memory option.
+
+# ??? save results to separate folder? --> /results/m
+
+# run sparkbench with pure disk option.
+
+# ??? save results to another separate folder? --> /results/d
---
+++ benchmarks/HiBench/setup.sh
@@ -0,0 +1,11 @@
+# check if Homebrew is installed.
+echo -e "Homebrew already installed..."
+echo -e "Homebrew not installed. Installing now..."
+
+# check for java version.
+
+# check for HiBench folder in Distributed_Memory folder.
+
+# build SparkBench.
+
+# download hadoop and spark.
