c4ca3ee419193ce70a7a16777246844588c6a6b3
Ubuntu <kyuseung1016@gmail.com>
2022-11-11 22:31:01 +0000

    remove comments

--- benchmarks/HiBench/run-partial.sh
+++ benchmarks/HiBench/run-partial.sh
@@ -9,28 +9,28 @@ function run-partial {
     SQL="aggregation join scan"
 
     # run micros
-    #for val in $MICRO; do
-    #    micro/$val/prepare/prepare.sh
-    #    micro/$val/spark/run.sh
-    #done
+    for val in $MICRO; do
+        micro/$val/prepare/prepare.sh
+        micro/$val/spark/run.sh
+    done
 
     # run graph
-    #for val in $GRAPH; do
-    #    graph/$val/prepare/prepare.sh
-    #    graph/$val/spark/run.sh
-    #done
+    for val in $GRAPH; do
+        graph/$val/prepare/prepare.sh
+        graph/$val/spark/run.sh
+    done
 
     # run streaming
-    #for val in $STREAMING; do
-    #    streaming/$val/prepare/prepare.sh
-    #    streaming/$val/spark/run.sh
-    #done
+    for val in $STREAMING; do
+        streaming/$val/prepare/prepare.sh
+        streaming/$val/spark/run.sh
+    done
 
     # run machine learning
-    #for val in $ML; do
-    #    ml/$val/prepare/prepare.sh
-    #    ml/$val/spark/run.sh
-    #done
+    for val in $ML; do
+        ml/$val/prepare/prepare.sh
+        ml/$val/spark/run.sh
+    done
 
     # run SQL
     for val in $SQL; do
